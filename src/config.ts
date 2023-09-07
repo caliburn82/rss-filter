71600f3358a3f9ef159a71f15183f6d73f4df5a5
Jon Allen <jonathan.allen@boohoo.com>
2023-09-07 11:34:45 +0100

    update default rss feed

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 const env = process.env;
 
 export const rss = {
-  url: env.RSS_URL || 'http://feeds.feedburner.com/crunchyroll/rss/anime',
+  url: env.RSS_URL || 'https://crunchyroll.com/rss/anime',
   keyWords: env.RSS_KEY_WORDS || 'English Dub',
 }
 
