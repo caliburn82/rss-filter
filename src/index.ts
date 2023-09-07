e10581a4cf5023c860b4b1dfb26e3be6c99dec0f
Jon Allen <jonathan.allen@boohoo.com>
2023-09-07 12:11:17 +0100

    Add missing object elements to feed

--- src/index.ts
+++ src/index.ts
@@ -21,44 +21,45 @@ async function getFeedContent() {
   return {result, headers};
 }
 
+const formatters: SIObject<Function> = {
+  string: (key: string, value: string) => `<${key}>${value}</${key}>`,
+  object: (key: string, value: SIObject<string>) => {
+    const attributes = Object.entries({...value})
+      .map(([key, value]) => `${key}="${value}"`)
+      .join(' ');
+
+    return `<${key} ${attributes} />`;
+  },
+  array: (key: string, value: any[]) => `<${key}>${value.join(', ')}</${key}>`,
+  default: (key: string, value: any) => `<!-- unknown: ${key} - ${typeof value}: ${JSON.stringify(value)} -->`,
+};
+
 function feedObjectToXml(feedObject: any): string {
   const items = feedObject.items
     .filter((item: any) => item.title.toLowerCase().includes(rss.keyWords.toLowerCase())) // filter by keywords
     .map((item: any) => {
       const itemHeaders = Object.entries(item)
-        // .filter(([key, value]) => typeof value === 'string')
         .map(([key, value]) => {
-          if (typeof value === 'string') {
-            return `    <${key}>${value}</${key}>`;
-          }
+          let type: string = typeof value;
+          if (type === 'object' && Array.isArray(value)) type = 'array';
+          formatters.hasOwnProperty(type) || (type = 'default');
 
-          // value to html attributes
-          if (typeof value === 'object') {
-            const attributes = Object.entries({...value})
-              .map(([key, value]) => `${key}="${value}"`)
-              .join(' ');
-
-            return `    <${key} ${attributes} />`;
-          }
-
-          return `<!-- unknown: ${key} - ${typeof value}: ${JSON.stringify(value)} -->`;
+          return `\t\t\t\t${formatters[type](key, value)}`;
         });
-      return `  <item>${itemHeaders.join('\n')}</item>`;
+      return `<item>\n${itemHeaders.join('\n')}\n\t\t</item>`;
     });
 
   const headers = Object.entries(feedObject)
     .filter(([key]) => key !== 'items')
-    .map(([key, value]) => {
-      return `  <${key}>${value}</${key}>`;
-    });
+    .map(([key, value]) => formatters.string(key, value));
 
-  return `<?xml version="1.0" encoding="UTF-8"?>
-    <rss version="2.0" xmlns:media="http://search.yahoo.com/mrss/" xmlns:crunchyroll="http://www.crunchyroll.com/rss">
-      <channel>
-        ${headers.join('\n')}
-        ${items.join('\n')}
-      </channel>
-    </rss>`;
+  return '<?xml version="1.0" encoding="UTF-8"?>' +
+    '<rss version="2.0" xmlns:media="http://search.yahoo.com/mrss/" xmlns:crunchyroll="http://www.crunchyroll.com/rss">\n' +
+    '\t<channel>\n' +
+    `\t\t${headers.join('\n')}\n` +
+    `\t\t${items.join('\n')}\n` +
+    '\t</channel>\n' +
+    '</rss>';
 }
 
 export async function handler(): Promise<Response> {
